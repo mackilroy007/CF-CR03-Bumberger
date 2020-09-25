f57c1474a222e5f30cc961c01baab3c9ec560d3a
mackilroy007 <mackilroy007@gmail.com>
2020-09-25 17:38:39 +0200

    claculation is finished and initial link with json file has been added

--- script.js
+++ script.js
@@ -6,29 +6,32 @@ var result = document.getElementById("result");
 
 function calculateInsurance() {
     var yourName = document.getElementById("yname").value;
-    var displayName = yourName[0].toUpperCase() + yourName.substring(1) 
+    var displayName = yourName[0].toUpperCase() + yourName.substring(1);
     var age = document.getElementById("age").value;
     var country = document.getElementById("country").value;
     var hpOfCar = document.getElementById("hpOfCar").value;
     var huf = 363.37; //conversion rate 25.09 placeholder
-    // console.log(firstName);
-    // console.log(age);
-    // console.log(country);
-    // console.log(hpOfCar);
+
     //using Math.round to round up the highest int if the number has decimal places
-    if (country == "Austria") { //Austria : insurance = horse_power x 100 / age   + 50;
-        var calc1 = (Math.round(hpOfCar * 100 / (age)) + 50);
-        result.innerHTML = displayName + ", our insurance rate offer will be " + calc1.toLocaleString() +" €";
+    if (country == "Austria" && age >= 18) { //Austria : insurance = horse_power x 100 / age   + 50;
+        var calcA = (Math.round(hpOfCar * 100 / (age)) + 50); //seperating the calculation from the display for easier access
+        result.innerHTML = "<br>" + displayName + ", our insurance rate offer will be " + calcA.toLocaleString() +" €";
         // console.log(result);
-    } else if (country == "Hungary") {//Hungary: insurance = horse_power x 120 / age  + 100;
-        var calc2 = ((Math.round(hpOfCar * 120 / (age) + 100))*huf);
-        result.innerHTML = displayName + ", our insurance rate offer will be " + calc2.toLocaleString() + " Ft";
+    } else if (country == "Hungary" && age >= 18) {//Hungary: insurance = horse_power x 120 / age  + 100;
+        var calcH = ((Math.round(hpOfCar * 120 / (age) + 100))*huf);
+        result.innerHTML = "<br>" + displayName + ", our insurance rate offer will be " + calcH.toLocaleString() + " Ft";
         // console.log(result);
-    } else {    //Greece: insurance = horse_power x 150 / (age+3)  + 50;
-        var calc3 = (Math.round(hpOfCar * 150 / (age+3) + 50));
-        result.innerHTML = displayName + ", our insurance rate offer will be " + calc3.toLocaleString() +" €";
+    } else if (country == "Greece" && age >= 18) {    //Greece: insurance = horse_power x 150 / (age+3)  + 50;
+        var calcG = (Math.round(hpOfCar * 150 / (age+3) + 50));
+        result.innerHTML = "<br>" +displayName + ", our insurance rate offer will be " + calcG.toLocaleString() +" €";
         // console.log(result);
+    } else {
+        result.innerHTML = `<br>We only offer insurance for individuals who are 18 or older.`
     }
 
 }
 document.getElementById("button").addEventListener("click",calculateInsurance,false);
+
+//linking the JSON file
+var employees = JSON.parse(teamMembers);
+console.table(teamMembers);
